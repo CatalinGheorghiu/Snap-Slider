202783bdf7e76dbd7e22d0df326d6f955e4275a5
CatalinGheorghiu <ionutcatalingheorghiu@yahoo.com>
2021-05-01 12:04:39 +0300

    add snap scroll

--- js/script.js
+++ js/script.js
@@ -8,17 +8,19 @@ let numberOfSlides = slide.length;
 let slideWidth = slide[0].clientWidth;
 let currentSlide = 0;
 
+
 function init() {
+	// Set the left position
 	slide.forEach((item, index) => {
 		item.style.left = `${index * 100}%`;
 	});
 	
+	//Add active class to first slide when page loads
 	slide[0].classList.add("active");
 	
 	createNavigationDots();
 }
 
-init();
 
 // Create Nav dots
 function createNavigationDots() {
@@ -27,17 +29,21 @@ function createNavigationDots() {
 		dot.classList.add("single-dot");
 		navigationDots.appendChild(dot);
 		
+		// Go to clicked slide
 		dot.addEventListener("click", () => {
 			goToSlide(i);
 		});
+		
 	}
-	
+	// Set the default active dot
 	navigationDots.children[0].classList.add("active");
 }
 
 //Next button
 nextBtn.addEventListener("click", () => {
+	//If current slide is the last slide
 	if (currentSlide >= numberOfSlides - 1) {
+		//Go to the first slide
 		goToSlide(0);
 		return;
 	}
@@ -47,7 +53,9 @@ nextBtn.addEventListener("click", () => {
 
 //Previous button
 prevBtn.addEventListener("click", () => {
+	// If current slide is the first slide
 	if (currentSlide <= 0) {
+		// Go to the last slide
 		goToSlide(numberOfSlides - 1);
 		return;
 	}
@@ -55,19 +63,37 @@ prevBtn.addEventListener("click", () => {
 	goToSlide(currentSlide);
 });
 
+// Slide Container Scroll
+slideContainer.addEventListener("scroll", () => {
+	// Check the number of pixels scrolled from the left side and divide by the slide width to get the current slide
+	currentSlide = Math.round(slideContainer.scrollLeft / slideWidth);
+	setActiveClass();
+});
+
+
 function goToSlide(slideNumber) {
-	slideContainer.style.transform = `translateX(-${slideWidth * slideNumber}px)`;
+	// Set the current slide on the screen
+	slideContainer.scrollTo({
+		left: slideWidth * slideNumber,
+		behavior: "smooth"
+	});
+	
 	currentSlide = slideNumber;
+	
 	setActiveClass();
 }
 
 function setActiveClass() {
+	// Set active class for the slide
 	let currentActive = document.querySelector(".slide.active");
 	currentActive.classList.remove("active");
 	slide[currentSlide].classList.add("active");
 	
-	// set active class for the dots
+	// Set active class for the dots
 	let currentDot = document.querySelector(".single-dot.active");
 	currentDot.classList.remove("active");
 	navigationDots.children[currentSlide].classList.add("active");
 }
+
+
+init();
